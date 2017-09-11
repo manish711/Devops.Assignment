1b6449d028dff1f91e2afdef6fdb3993351382c5
Manish Narang <manish.narang@nagarro.com>
2017-09-11 16:53:05 +0530

    Added: HelloWorld Project + Unit Test Project

---
+++ src/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 2;
+            Console.WriteLine("Hello World");
+            Program.Add(x, y);
+            Console.Read();
+        }
+
+        public static void Add(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+    }
+}
